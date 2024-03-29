2cc25a6d88f80affeb1c70447bd1efe82ae35e8a
Braydon Jones <bradofrado@gmail.com>
2024-03-29 02:16:45 +0000

    Update files content

--- packages/ui/ContentWrapper/index.tsx
+++ packages/ui/ContentWrapper/index.tsx
@@ -6,5 +6,5 @@ interface ContentWrapperProps {
 }
 
 export default function ContentWrapper({ children, className }: ContentWrapperProps) {
-  return <div className={cn("mx-auto max-w-7xl p-6", className)}>{children}</div>;
+  return <div /** pl-6 pt-12 pb-6 */ className={cn("mx-auto max-w-7xl p-6", className)}>{children}</div>;
 }
