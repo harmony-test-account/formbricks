fb7122d4ae47902071d492f1bc234576ff1a730b
Braydon Jones <bradofrado@gmail.com>
2024-03-29 02:16:45 +0000

    Update files content

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/components/CustomFilter.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/components/CustomFilter.tsx
@@ -204,7 +204,7 @@ const CustomFilter = ({ environmentTags, attributes, survey }: CustomFilterProps
 
   return (
     <>
-      <div className="relative mb-6 flex gap-x-1.5 py-5">
+      <div className="relative flex gap-x-1.5 py-5 mr-0 mt-12 mb-0">
         <ResponseFilter />
         <DropdownMenu
           onOpenChange={(value) => {
