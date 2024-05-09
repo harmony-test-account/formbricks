23bb93518003d5441e109b6d1063edd6f2504291
Braydon Jones <bradofrado@gmail.com>
2024-05-09 21:30:52 +0000

    Update files content

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/components/CustomFilter.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/components/CustomFilter.tsx
@@ -267,7 +267,7 @@ const CustomFilter = ({ environmentTags, attributes, survey }: CustomFilterProps
             setIsDownloadDropDownOpen(value);
           }}>
           <DropdownMenuTrigger className="flex h-auto items-center justify-between rounded-md border border-slate-200 bg-white p-3 hover:border-slate-300 sm:px-6 sm:py-3">
-            <span className="text-sm text-slate-700">Download</span>
+            <span className="text-sm text-slate-700">Uploads</span>
             {isDownloadDropDownOpen ? (
               <ChevronUp className="ml-2 h-4 w-4 opacity-50" />
             ) : (
